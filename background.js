feeaec1acd41c085a8cfd904e9940998a1246843
PIAI20DEC4012 <habubakar89@gmail.com>
2021-02-09 12:18:32 +0530

    First commit

---
+++ background.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  chrome.tabs.query({}, function(tabs) { /* blah */ } );
+  console.log("tabs",tabs);
+  const tabsInfo = tabs.map(tab => ({
+    title: tab.title,
+    url: tab.url
+  }));
+  console.log("tabsInfo",tabsInfo);
+  /*
+  
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="blue"'
+  });*/
+});
